f137abd5ab1a06ade6eee3e3575e6e8769f49cb9
Shawn Kristek <shawn@wieck.com>
2025-12-05 10:45:44 -0600

    enable folding by default

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.foldenable = true
+o.foldmethod = "syntax"
+o.foldlevelstart = 99
